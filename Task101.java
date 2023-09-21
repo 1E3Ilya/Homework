9c7ec0a432af48d78c346cb5752e37a04d4bb0e4
1E3Ilya <144244638+1E3Ilya@users.noreply.github.com>
2023-09-21 21:12:16 +0300

    Add files via upload

---
+++ Task101.java
@@ -0,0 +1,17 @@
+public class Task101 {
+	public static void main(String[] args){
+		int n = 10;
+		int i;
+		int j;
+		for (i = 0; i <= n; i++) {
+			for (j = i; j < n; j++) {
+				System.out.print("*");
+			}
+		System.out.println();
+		}
+	}
+}
+		
+    
+ 
+	
---
+++ Task9.java
@@ -0,0 +1,14 @@
+public class Task9 {
+	public static void main(String[] args){
+		int n = 3;
+		int m = 7;
+		int i;
+		int j;
+		for (j=0;j<m;j+=1){
+			for (i = 0; i<n; i+=1){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
